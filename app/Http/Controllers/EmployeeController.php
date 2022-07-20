5252f93fe045c32dd1c4d69a3f9236065816f050
Mohammed Alani <aalani92@yahoo.com>
2022-07-20 15:06:48 +0300

    change readme

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -15,13 +15,8 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        // get all employees from database and sum their salary with group by city
-        // $employees = Employee::select(\DB::raw('CONCAT_WS(" ",first_name, last_name) as full_name'),
-        // 'city', \DB::raw('sum(salary) as total_salary'))
-        //     ->groupBy('city')
-        //     ->get();
-
         $employees = \DB::select("SELECT city, salary, GROUP_CONCAT(CONCAT_WS(' ',first_name, last_name) SEPARATOR ',') AS full_name, sum(salary) as total_salary FROM employees GROUP BY city");
+
         return response()->json($employees);
     }
 
